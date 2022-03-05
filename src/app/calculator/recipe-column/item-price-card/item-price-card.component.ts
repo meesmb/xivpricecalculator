575d16a4ac8abc13af2c1708af91eebc5f460ae5
meesmb <60484127+meesmb@users.noreply.github.com>
2022-03-05 12:19:08 +0100

    removed description from item cards and added links to universalis and teamcraft

--- src/app/calculator/recipe-column/item-price-card/item-price-card.component.ts
+++ src/app/calculator/recipe-column/item-price-card/item-price-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TransformedItem} from "../../../../models/transformed-item.model";
 import {ClipboardService} from "ngx-clipboard";
+import { Base64 } from 'js-base64';
 
 @Component({
   selector: 'app-item-price-card',
@@ -48,11 +49,24 @@ export class ItemPriceCardComponent implements OnInit {
     if (this.item.i === null || !this.shouldShowToggle) return false;
     return this.item.i.isCraftedItem()
   }
+
   getRecipeIconUrl() : string {
     return "https://xivapi.com" + this.item.i?.getResultItem().getIcon();
   }
 
+  getTeamCraftLink() : string {
+    let s = this.item.i?.getResultItem().getID() + ",null,1";
+    return "https://ffxivteamcraft.com/import/" + Base64.encode(s);
+  }
+  getUniversalisLink() : string {
+    return "https://universalis.app/market/" + this.item.i?.getResultItem().getID();
+  }
+
   toggle() {
     this.onToggleCraft.emit(this.item);
+  }
+  getIsCraftedItem() : boolean {
+    if (this.item.i) return this.item.i.isCraftedItem();
+    return false;
   }
 }
